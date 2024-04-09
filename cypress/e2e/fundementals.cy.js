1033cf42d79e3fa7f54364c1cb514d0c446b2782
deborah <deborahberhanu644@gmail.com>
2024-04-09 21:14:20 +0300

    fundementals of cypress

--- cypress/e2e/fundementals.cy.js
+++ cypress/e2e/fundementals.cy.js
@@ -1,10 +1,11 @@
 describe('Fundementals test', () => {
+  beforeEach(() =>{
+       cy.visit('/fundamentals')
+  })
   it('Contains correct header text', () => {
-    cy.visit('/fundamentals')
-    cy.get('[data-test="fundementals-header"]').should('contain.text', 'Testing Fundamentals')
+    cy.getDataTest('fundementals-header').should('contain.text', 'Testing Fundamentals')
   })
   it('Accordion works correctly', () => {
-    cy.visit('/fundamentals')
     cy.contains(/Your tests will exist in a describe block./i).should('not.be.visible')
     cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
     cy.contains(/Your tests will exist in a describe block./i).should('be.visible')
